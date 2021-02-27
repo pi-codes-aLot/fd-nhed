daa22e543f6c545dd7d866af2e48f61f65e3c016
pi-codes-aLot <72666455+pi-codes-aLot@users.noreply.github.com>
2021-02-27 11:42:26 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var cImg1, cImg2, cImg3, cImg4, groundImg, trackImg;
+
+function preload(){
+cImg1 = loadImage("images/car1.png");
+cImg2 = loadImage("images/car2.png");
+cImg3 = loadImage("images/car3.png");
+cImg4 = loadImage("images/car4.png");
+groundImg = loadImage("images/ground.png");
+trackImg = loadImage("images/track.png");
+}
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState == 2){
+    game.end()
+  }
+}
